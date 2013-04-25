11bce22a440dea9453d78e7dc040805fc9ef525d
Jacob Poul Richardt <spam@poulrichardt.dk>
2013-04-25 16:27:24 +0200

    Added test schema page.

--- src/app/CHAOS.Portal.Mimir/App/viewmodels/MetadataSchemas.js
+++ src/app/CHAOS.Portal.Mimir/App/viewmodels/MetadataSchemas.js
@@ -1,5 +1,15 @@
 define(["require", "exports"], function(require, exports) {
+    exports.Schemas = ko.observableArray();
     function activate() {
+        CHAOS.Portal.Client.MetadataSchema.Get().WithCallback(SchemaGetComplated);
     }
     exports.activate = activate;
+    function SchemaGetComplated(response) {
+        if(response.Error != null) {
+            throw response.Error.Message;
+        }
+        for(var i = 0; i < response.Result.Results.length; i++) {
+            exports.Schemas.push(response.Result.Results[i]);
+        }
+    }
 })
--- src/app/CHAOS.Portal.Mimir/App/viewmodels/MetadataSchemas.ts
+++ src/app/CHAOS.Portal.Mimir/App/viewmodels/MetadataSchemas.ts
@@ -1,7 +1,19 @@
 /// <reference path="../TypeScriptDefinitions/require.d.ts" />
 /// <reference path="../TypeScriptDefinitions/durandal.d.ts" />
+/// <reference path="../TypeScriptDefinitions/PortalClient.d.ts" />
+
+export var Schemas: KnockoutObservableArray = ko.observableArray();
 
 export function activate()
 {
+	CHAOS.Portal.Client.MetadataSchema.Get().WithCallback(SchemaGetComplated);
+}
 
+function SchemaGetComplated(response:CHAOS.Portal.Client.IPortalResponse):void
+{
+	if (response.Error != null)
+		throw response.Error.Message;
+
+	for (var i: number = 0; i < response.Result.Results.length; i++)
+		Schemas.push(response.Result.Results[i]);
 }
